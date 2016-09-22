7ad8ff6244c84c83bc3dbf04e0c29c67163968ea
Sri <sridharkaza@yahoo.com>
2016-09-22 11:43:53 -0700

    adjusted UI slightly

--- src/main/webapp/js/admin_scripts.js
+++ src/main/webapp/js/admin_scripts.js
@@ -800,6 +800,14 @@ function getScoreUuid(respondantUuid) {
 	});    
 }
 
+function copyToClipboard(element) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($(element).text()).select();
+    document.execCommand("copy");
+    $temp.remove();
+}
+
 function presentRespondantScores(dataScores) {
 	$('#candidatename').text(respondant.respondant_person_fname + ' ' + respondant.respondant_person_lname);
 	$('#candidateemail').text(respondant.respondant_person_email);
